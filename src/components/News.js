05ef18d1dd817c5ae6c9d6c062522233539c943c
Rida702 <ai22c0rida@gmail.com>
2024-09-25 19:46:39 +0500

    Fetch data from News API

--- src/components/News.js
+++ src/components/News.js
@@ -2,14 +2,64 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 
 export class News extends Component {
+  constructor() {
+    super();
+    this.state = {
+      articles : [],
+      loading : false,
+      page : 1,
+      totalresults : 0
+    }
+  }
+  async componentDidMount(page){
+    let url_endpoint = `https://newsapi.org/v2/top-headlines?country=us&apiKey=c9414ea6183044b38934f41098922bcb&page=${this.page}&pageSize=20`;
+    let data = await fetch(url_endpoint);
+    let parsedData = await data.json();
+    console.log(parsedData);
+    this.setState({
+      articles: parsedData.articles, 
+      totalresults : parsedData.totalResults
+    })
+  }
+
+  handelPreviousClick = async() => {
+    let url_endpoint = `https://newsapi.org/v2/top-headlines?country=us&apiKey=c9414ea6183044b38934f41098922bcb&page=${this.state.page - 1}&pageSize=20`;
+    let data = await fetch(url_endpoint);
+    let parsedData = await data.json();
+    console.log(parsedData);
+    this.setState({
+      page : this.state.page - 1
+    })
+  }
+
+  handelNextClick = async () => {
+    let url_endpoint = `https://newsapi.org/v2/top-headlines?country=us&apiKey=c9414ea6183044b38934f41098922bcb&page=${this.state.page + 1}&pageSize=20`;
+    let data = await fetch(url_endpoint);
+    let parsedData = await data.json();
+    console.log(parsedData);
+    this.setState({
+      page : this.state.page + 1,
+      articles: parsedData.articles
+      })
+  }
+
   render() {
+    console.log("Render")
     return (
-      <div>
-        This is a News Component.
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
+      <div className='container my-3'  >
+        <h2>NewMonkey - Top Headlines</h2>
+        <div className="row">
+        {this.state.articles.filter((element)=> element.title !== "[Removed]" && element.description !== "[Removed]").map((element)=>{
+          return <div className="col-md-4" key={element.url?element.url:"/"}>
+            <NewsItem title={element.title?element.title:""} description={element.description?element.description:""} 
+            imageUrl={element.urlToImage?element.urlToImage:"https://cdn.mos.cms.futurecdn.net/mLsWb5UBE2sX9AkoKSrCri-1200-80.jpg"} newsUrl={element.url?element.url:"/"} />
+          </div>
+        })}
+        </div>
+        <div className="container d-flex justify-content-between">
+        <button disabled={this.state.page<=1} type="button" className="btn btn-dark" onClick={this.handelPreviousClick}> &larr;Previous</button>
+        <button disabled={this.state.page + 1 > Math.ceil(this.state.totalresults / 20)} type="button" className="btn btn-dark" onClick={this.handelNextClick}>Next&rarr;</button>
+        </div>
       </div>
     )
   }
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,9 +2,17 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
   render() {
+    let { title, description, imageUrl, newsUrl } = this.props
     return (
-      <div>
-        This is a News Item.
+      <div className="my-3">
+        <div className="card" style={{ width: "18rem" }}>
+          <img src={!imageUrl?"https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg":imageUrl} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
+            <a href={newsUrl} target="_blank" rel="noopener noreferrer" className="btn btn-sm btn-dark">Read More</a>
+            </div>
+        </div>
       </div>
     )
   }
